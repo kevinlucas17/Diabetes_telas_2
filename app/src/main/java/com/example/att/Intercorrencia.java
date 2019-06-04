36a108930542b3b9378bc430ab9c005faf5d05b7
kevinlucas17 <49851300+kevinlucas17@users.noreply.github.com>
2019-06-03 23:37:54 -0300

    Initial commit

---
+++ app/src/main/java/com/example/att/Intercorrencia.java
@@ -0,0 +1,14 @@
+package com.example.att;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Intercorrencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intercorrencia);
+        setTitle("Intercorrência");
+    }
+}
